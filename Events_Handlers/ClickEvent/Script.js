2929c4e3183f1a1199544e2680d7e4f4f5b3e5c7
AshishRaj5191 <ashish.ara2017@gmail.com>
2026-04-14 16:40:36 +0530

    Working with click

---
+++ Events_Handlers/ClickEvent/Script.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('mybtn');
+
+//Event
+// btn.onclick = () => {
+//     alert("Button Clicked")
+// }
+
+// btn.addEventListener('click', function() {
+//     alert('Button Clicked');
+// });
+
+btn.addEventListener('click', () => {
+    alert('Button Clicked');
+});
