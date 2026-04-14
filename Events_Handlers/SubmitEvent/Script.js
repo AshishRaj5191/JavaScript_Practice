6c71ede5a419084be482c7c9c71fe11b35adb392
AshishRaj5191 <ashish.ara2017@gmail.com>
2026-04-14 19:13:11 +0530

    Working with submit

---
+++ Events_Handlers/SubmitEvent/Script.js
@@ -0,0 +1,7 @@
+let form = document.getElementById('formid');
+
+form.addEventListener('submit', (e) => {
+    // e.preventDefault();
+    // alert("The Form has been submitted successfully ✔️");
+    console.log("The Form has been submitted successfully ✔️");
+});
